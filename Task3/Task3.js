407c25199963d0a1dcf502c1f38f68e473f02a32
itzprem12 <itz.a.prem@gmail.com>
2024-08-18 19:07:40 +0530

    Add files via upload

---
+++ Task3/Task3.js
@@ -0,0 +1,17 @@
+import React from "react";
+export const Task1 = () => {
+  return (
+    <>
+  <div className="Container">
+    <h1 className="Main">Congratulations</h1>
+    <div className="insideCard">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" className="img01"/>
+      <h2 className="Title">Prem</h2>
+      <p className="para">KG College of Arts and Science</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+  </>
+  )
+};
+
